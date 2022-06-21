b51cfe72a8a77b2447eafbce67dc54479257db76
Bida <luabidaa@gmail.com>
2022-06-21 14:59:34 -0300

    Define straight and flush

--- lesson_1/card_ranks.py
+++ lesson_1/card_ranks.py
@@ -10,13 +10,13 @@ def card_ranks(cards):
     "Return a list of the ranks, sorted with higher first."
     ranks = [r for r,s in cards]
     figures = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
-    a = []
+    formated_ranks = []
     for rank in ranks:
         if rank in figures:
-            a.append(figures[rank])
+            formated_ranks.append(figures[rank])
         else:
-            a.append(int(rank))
-    a.sort(reverse=True)
-    return a
+            formated_ranks.append(int(rank))
+    formated_ranks.sort(reverse=True)
+    return formated_ranks
 
 print(card_ranks(['AC', '3D', '4S', 'KH'])) #should output [14, 13, 4, 3]
---
+++ lesson_1/straight_flush.py
@@ -0,0 +1,43 @@
+# -----------
+# User Instructions
+# 
+# Define two functions, straight(ranks) and flush(hand).
+# Keep in mind that ranks will be ordered from largest
+# to smallest.
+
+def straight(ranks) -> bool:
+    "Return True if the ordered ranks form a 5-card straight."
+    ranks_s = sorted(ranks)
+    avg = int(sum(ranks) / 5)
+    if avg == ranks_s[2]:
+        return True
+    return False
+
+def flush(hand) -> bool:
+    "Return True if all the cards have the same suit."
+    suits = [s for r,s in hand]
+    suits_rank = {'D': 1, 'S': 2, 'H': 3, 'C': 4}
+    suits_form = []
+    for suit in suits:
+        suits_form.append(suits_rank[suit])
+    avg = int(sum(suits_form) / 5)
+    if avg == suits_rank[suit]:
+        return True
+    return False
+    
+
+def test():
+    "Test cases for the functions in poker program."
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    f = "TD 2D 5D 7D QD".split()
+    assert straight([9, 8, 7, 6, 5]) == True
+    assert straight([9, 8, 8, 6, 5]) == False
+    assert straight([2, 6, 3, 4, 5]) == True
+    assert flush(f) == True
+    assert flush(sf) == True
+    assert flush(fk) == False
+    return 'tests pass'
+
+print(test())
