96e179141635c3334f39afc10453edcdd458239d
Bida <luabidaa@gmail.com>
2022-06-22 14:39:11 -0300

    Starting with list comprehentions

---
+++ lesson_2/list_comprehention.py
@@ -0,0 +1,24 @@
+udacity_tas = ['Peter', 'Andy', 'Sarah', 'Gundega', 'Job', 'Sean']
+
+bad_uppercase_tas = []
+for i in range(len(udacity_tas)):
+    bad_uppercase_tas.append(udacity_tas[i].upper())
+
+print(bad_uppercase_tas)
+
+# Solution in List Comprehention:
+
+uppercase_tas = [i.upper() for i in udacity_tas]
+
+def test():
+    assert uppercase_tas == [
+        'PETER', 
+        'ANDY', 
+        'SARAH', 
+        'GUNDEGA', 
+        'JOB', 
+        'SEAN'
+        ]
+    print("Tests Passed!")
+
+print(test())
