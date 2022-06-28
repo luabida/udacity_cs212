b4dd2df66772e6ead8bc622d51d82072162a2463
Bida <luabidaa@gmail.com>
2022-06-28 09:11:44 -0300

    Extracting letters from formula

---
+++ lesson_2/1.8.1_fill_in.py
@@ -0,0 +1,32 @@
+# -------------
+# User Instructions
+#
+# Complete the fill_in(formula) function by adding your code to
+# the two places marked with ?????. 
+
+import re, itertools
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    for f in fill_in(formula):
+        if valid(f):
+            yield f
+    
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(set(re.findall('\w', formula))) #should be a string
+    for digits in itertools.permutations('1234567890', len(letters)):
+        table = str.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
+    
+def valid(f):
+    """Formula f is valid if and only if it has no 
+    numbers with leading zero, and evals true."""
+    try: 
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
+
+for i in solve("ODD + ODD == EVEN"):
+    print(i)
